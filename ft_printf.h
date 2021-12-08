228cfa3a345fcf5596c75d05748140c3332765d2
nandajfa <nanda.jfa@gmail.com>
2021-12-08 18:02:34 -0300

    header

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jefernan <jefernan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 08:30:51 by jefernan          #+#    #+#             */
-/*   Updated: 2021/12/08 17:18:25 by jefernan         ###   ########.fr       */
+/*   Updated: 2021/12/08 18:01:31 by jefernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include "libft/libft.h"
 # include <stdarg.h>
 
-int printf(const char *format, ...);
+int ft_printf(const char *format, ...);
 int	ft_print_str(char *str);
 int	ft_print_d_i(int nbr);
 int	ft_print_unsigned(unsigned int nbr);
